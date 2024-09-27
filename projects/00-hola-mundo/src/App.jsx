3e2a72c3772c451092f09be72ebb05e9a815b43b
Jairo <jairomotasilva@icloud.com>
2024-09-27 23:01:49 +0200

    Add first class

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { XFollowCard } from "./XFollowCard";
 
-function App() {
-  const [count, setCount] = useState(0)
+const users = [
+  {
+    userName: 'JairoMS27',
+    name: 'Ejemplo',
+    isFollowing: false
+  },
+  {
+    userName: 'midudev',
+    name: 'Miguel Angel Duran',
+    isFollowing: true
+  },
+  {
+    userName: 'PacoHdezs',
+    name: 'Paco Hdez',
+    isFollowing: false
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo H.',
+    isFollowing: true
+  },
+]
 
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <section className="App">
+      {
+        users.map(({userName, name, isFollowing}) => (
+            <XFollowCard
+              key={userName}
+              userName={userName}
+              initialIsFollowing={isFollowing}
+            >
+              {name}
+            </XFollowCard>
+          )
+        )
+      }
+    </section>
+  );
 }
-
-export default App
